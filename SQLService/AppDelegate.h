acf6a2a3ba889f7913cabc770cc01561e57d6883
zkil <97100170@qq.com>
2016-02-03 15:01:00 +0800

    Initial Commit

---
+++ SQLService/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQLService
+//
+//  Created by lee on 16/2/3.
+//  Copyright © 2016年 sanchun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQLService/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQLService
+//
+//  Created by lee on 16/2/3.
+//  Copyright © 2016年 sanchun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
